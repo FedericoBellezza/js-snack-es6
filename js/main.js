33199c4d58d093e3f9335dcd574407142a5b570b
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-25 16:15:34 +0200

    error

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+console.log('-------------------------------- SNACK 1 --------------------------------');
+
 const bikes = [
 	{
 		name: 'Bici A',
@@ -49,10 +51,68 @@ console.log(`La Bici che pesa di meno è la "${minWeightBikeIndex.name}", che pe
 
 // - Snack 2
 // Creare un array di oggetti di squadre di calcio.
-//  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+console.log('-------------------------------- SNACK 2 --------------------------------');
+
+const teams = [
+	{
+		name: 'Juventus',
+		points: 0,
+		fouls: 0,
+	},
+	{
+		name: 'Inter',
+		points: 0,
+		fouls: 0,
+	},
+	{
+		name: 'Milan',
+		points: 0,
+		fouls: 0,
+	},
+	{
+		name: 'Cagliari',
+		points: 0,
+		fouls: 0,
+	},
+	{
+		name: 'Hellas Verona',
+		points: 0,
+		fouls: 0,
+	},
+	{
+		name: 'Fiorentina',
+		points: 0,
+		fouls: 0,
+	},
+	{
+		name: 'Como',
+		points: 0,
+		fouls: 0,
+	},
+];
+
+// - Random number generation function
+function randomNumGeneration(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+for (const i in teams) {
+	teams[i].points = randomNumGeneration(0, 10);
+	teams[i].fouls = randomNumGeneration(0, 10);
+}
+const newArray = [...teams];
+
+for (const i in newArray) {
+	delete newArray[i].points;
+}
+
+console.log('Array completo:');
+console.table(teams);
+console.table(newArray);
+
 // - Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
